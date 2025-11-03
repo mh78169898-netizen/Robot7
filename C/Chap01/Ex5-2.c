2449f092dba6886ded6a6a4191836259a092d5f2
mh78169898-netizen <mh78169898@gmail.com>
2025-11-03 10:57:30 +0900

    Ex5-2.c
    5-2.c

---
+++ C/Chap01/Ex5-2.c
@@ -0,0 +1,18 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = -7;
+//
+//	if (a >= 0) {
+//		a = 1;
+//	}
+//	else {
+//		a = -1;
+//	}
+//
+//	printf("a : %d\n", a);
+//
+//	return 0;
+//}
