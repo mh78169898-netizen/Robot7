b90b6e1bef6fa18c64dd9e1a441977bb236063dd
mh78169898-netizen <mh78169898@gmail.com>
2025-11-06 10:40:51 +0900

    Create Ex9-2.c

---
+++ C/Chap01/pointer/Ex9-2.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	int* pa;
+
+	pa = &a; //a변수의 시작주소를 pa에 넣는다.(int*)
+	*pa = 10;
+
+	printf("*pa변수의 주소값 : %u\n", &pa);
+	printf("a변수의 주소값 : %u\n",&a);
+	printf("*pa변수의 값 : %u\n\n", pa);
+
+	printf("포인터로 a값 : %d\n", *pa);
+	printf("변수형으로 a값 : %d\n", a);
+	return 0;
+}
