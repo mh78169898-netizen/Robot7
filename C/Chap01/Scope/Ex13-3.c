019cce0f38aee3906007889102e3346ee65a637b
mh78169898-netizen <mh78169898@gmail.com>
2025-11-07 15:45:25 +0900

    Create Ex13-3.c

---
+++ C/Chap01/Scope/Ex13-3.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void assign10();
+void assign20();
+
+int a;
+
+int main()
+{
+
+	printf("함수 호출 전 a 값: %d\n", a);
+
+	assign10();
+	assign20();
+	
+	printf("함수 호출 후 a 값: %d\n", a);
+
+	return 0;
+}
+
+void assign10() {
+	a = 10;
+}
+
+void assign20() {
+	int a;
+
+	a = 20;
+}
+
