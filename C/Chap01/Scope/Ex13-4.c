571f11e81f4755ce38899e8f4b3f94e2bda94e80
mh78169898-netizen <mh78169898@gmail.com>
2025-11-07 15:53:36 +0900

    Create Ex13-4.c

---
+++ C/Chap01/Scope/Ex13-4.c
@@ -0,0 +1,34 @@
+//auto(일반)지역 변수와 static(정적)지역 변수의 비교
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void auto_func();//일반
+void static_func();//정적
+
+int main()
+{
+	int i;
+
+	printf("일반 지역 변수(auto)를 사용한 함수... \n");
+	for (i = 0; i < 3; i++) {
+		auto_func();
+	}
+
+	printf("정적 지역 변수(static)를 사용한 함수... \n");
+	for (i = 0; i < 3; i++) {
+		static_func();
+	}
+	return 0;
+}
+
+void auto_func() {
+	auto int a = 0;
+	a++;
+	printf("%d\n", a);
+}
+
+void static_func() {
+	static int a = 0;
+	a++;
+	printf("%d\n", a);
+}
