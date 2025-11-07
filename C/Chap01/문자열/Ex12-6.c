08654288309d9e3819b540894dd2f7b395346496
mh78169898-netizen <mh78169898@gmail.com>
2025-11-07 12:02:52 +0900

    Update Ex12-6.c

--- C/Chap01/문자열/Ex12-6.c
+++ C/Chap01/문자열/Ex12-6.c
@@ -14,5 +14,17 @@ int main()
 	gets(name);
 	printf("나이 : %d, 이름 : %s\n", age, name);
 
+	///////////////////fgets 함수 사용/////////////////////////////
+	
+	char sAge[10];
+	char name[20];
+
+	printf("나이 입력 : ");
+	fgets(sAge, 10, stdin); //17 --> "17
+	int age = atoi(sAge);
+
+	printf("이름 입력 : ");
+	fgets(name, 20, stdin);
+	printf("이름 : % s\n", name);
 	return 0;
 }
