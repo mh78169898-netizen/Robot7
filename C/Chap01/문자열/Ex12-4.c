a61d4564dc70f08ec08655ba14fa13ccda92b4e0
mh78169898-netizen <mh78169898@gmail.com>
2025-11-07 11:45:20 +0900

    Update Ex12-4.c

--- C/Chap01/문자열/Ex12-4.c
+++ C/Chap01/문자열/Ex12-4.c
@@ -7,7 +7,7 @@ int main()
 	char str[80];
 
 	printf("공백이 포함된 문자열 입력 : ");
-	gets(str);//Linux등 에서는 사용 불가능한 함수,C11버전에서 부터는 사용 불가
+	gets(str);//Linux등 에서는 사용 불가능한 함수,C11버전에서 부터는 사용 불가,fgets를 사용해야 함
 	printf("입력한 문자열은 %s입니다.\n", str);
 
 	return 0;
