fe8f3662ff921e0a369118d122393abd4f9b5036
mh78169898-netizen <mh78169898@gmail.com>
2025-11-10 12:21:06 +0900

    Create Ex14-1.c

---
+++ C/Chap01/이차원 배열/Ex14-1.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int score[3][4]; //y행,x행
+	int tot;
+	double avg;
+	int i, j;
+
+	for (int i = 0; i < 3; i++) {
+		printf("4과목의 점수 입력 : ");
+		for (int j = 0; j < 4; j++) {
+			scanf("%d", &score[i][j]);
+		}
+	}
+	//출력
+	for (int i = 0; i < 3; i++) {
+		tot = 0;
+		for (int j = 0; j < 4; j++) {
+			tot += score[i][j];
+		}
+		avg = tot / 4.0;
+		printf("총점 : %d, 평균 : %.2f\n", tot, avg);
+	}
+	return 0;
+}
