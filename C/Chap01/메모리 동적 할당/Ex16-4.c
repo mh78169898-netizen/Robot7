dba67667a6c5de9c9bd482e5eda59135d54f7b65
mh78169898-netizen <mh78169898@gmail.com>
2025-11-11 12:18:44 +0900

    Create Ex16-4.c

---
+++ C/Chap01/메모리 동적 할당/Ex16-4.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+//#include <memory.h>
+
+int main()
+{
+	char temp[80];
+	char* str[3];
+
+	for (int i = 0; i < 3; i++) {
+		printf("문자열을 입력하세요 : ");
+		fgets(temp, sizeof(temp), stdin);
+		str[i] = (char*)malloc(strlen(temp) + 1); //\0문자가 들어가는 곳
+		strcpy(str[i], temp);
+	}
+
+	for (int i = 0; i < 3; i++) {
+		printf("%s\n", str[i]);
+	}
+	for (int i = 0; i < 3; i++) {
+		free(str[i]);//메모리 해제
+	}
+
+	return 0;
+}
