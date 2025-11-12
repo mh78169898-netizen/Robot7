942dfcaa6c3db230d876899b0b81eff56ab09a50
mh78169898-netizen <mh78169898@gmail.com>
2025-11-12 09:50:31 +0900

    Update Q24.c

--- C/Chap01/퀴즈 복습/Q24.c
+++ C/Chap01/퀴즈 복습/Q24.c
@@ -57,44 +57,22 @@ int main()
 2.
 
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
-
 #include <string.h>
-
 struct cracker
-
 {
-
 	int price;
-
 	int cal;
-
 };
-
 int main()
-
 {
-
 	struct cracker c;
-
 	int price, cal;
-
 	printf("바사삭의 가격과 열랑을 입력하세요 : ");
+	scanf("%d %d", &c.price, &c.cal);
 
-	scanf("%d %d", &price, &cal);
-
-
-
-	printf("바사삭의 가격 : %d원\n", price);
-
-	printf("바사삭의 가격 : %dkcal\n", cal);
-
-
-
-
+	printf("바사삭의 가격 : %d원\n",c.price);
+	printf("바사삭의 가격 : %dkcal\n",c.cal);
 
 	return 0;
-
 }
-
