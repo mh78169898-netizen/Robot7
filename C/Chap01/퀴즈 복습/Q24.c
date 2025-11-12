c4323e02557c168ebcc16c7f06d50a5a7326f259
mh78169898-netizen <mh78169898@gmail.com>
2025-11-12 09:42:19 +0900

    Create Q24.c

---
+++ C/Chap01/퀴즈 복습/Q24.c
@@ -0,0 +1,100 @@
+1.
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#include <string.h>
+
+struct book
+
+{
+
+	char title[50];
+
+	char author[30];
+
+	int page;
+
+	int price;
+
+};
+
+int main()
+
+{
+
+	struct book a;
+
+
+
+	strcpy(a.title, "C프로그래밍");
+
+	strcpy(a.author, "데니스");
+
+	a.page = 180;
+
+	a.price = 18000;
+
+
+
+	printf("도서명 : %s\n", a.title);
+
+	printf("저자 : %s\n", a.author);
+
+	printf("페이지 수 : %d\n", a.page);
+
+	printf("가격 : %d\n", a.price);
+
+
+
+	return 0;
+
+}
+
+
+
+2.
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#include <string.h>
+
+struct cracker
+
+{
+
+	int price;
+
+	int cal;
+
+};
+
+int main()
+
+{
+
+	struct cracker c;
+
+	int price, cal;
+
+	printf("바사삭의 가격과 열랑을 입력하세요 : ");
+
+	scanf("%d %d", &price, &cal);
+
+
+
+	printf("바사삭의 가격 : %d원\n", price);
+
+	printf("바사삭의 가격 : %dkcal\n", cal);
+
+
+
+
+
+	return 0;
+
+}
+
