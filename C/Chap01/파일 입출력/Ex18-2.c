1ea8d7a20edd15a6a346528fa64326d8eff86e13
mh78169898-netizen <mh78169898@gmail.com>
2025-11-13 10:02:12 +0900

    Update Ex18-2.c

--- C/Chap01/파일 입출력/Ex18-2.c
+++ C/Chap01/파일 입출력/Ex18-2.c
@@ -22,3 +22,28 @@ int main()
 
     return 0;
 }
+///////////////////////////////수정 버전//////////////////////////////////
+#include <stdio.h>
+
+int main()
+{
+    FILE* fp;
+    int ch;
+    char* path = "/home/serrobot/work/basic/a.txt";
+    //char* mode = "r";
+    fp = fopen(path, "r"/*,mode*/);
+    if(fp == NULL){
+        printf("파일이 열리지 않았습니다.\n");
+        return 1;
+    }
+    while(1){
+        ch = fgetc(fp); //한 글자를 가져오는 함수
+        if(ch == EOF){ //-1 파일의 끝
+            break;
+        }
+        putchar(ch);//printf("%c", ch)
+    }
+    fclose(fp);//자원 반납(반환)
+
+    return 0;
+}
