26ec2b1bc3f723781c58fffd265fd7b6f15cdbf0
mh78169898-netizen <mh78169898@gmail.com>
2025-11-17 11:22:21 +0900

    Create Ex19-1.c

---
+++ C/Chap01/전처리와 분할 컴파일/Ex19-1.c
@@ -0,0 +1,21 @@
+//사용자 정의 헤더 파일을 사용하는 프로그램
+1.헤더 코드
+typedef struct
+{
+	int num;
+	char name[20];
+} Student;
+////////////////////////////////////////////////////
+2.메인 코드
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "student.h"
+
+int main()
+{
+	Student a = { 315,"홍길동" };
+
+	printf("학번 : %d, 이름 : %s\n", a.num, a.name);
+
+	return 0;
+}
