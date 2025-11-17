193e4d6b15150ae8470871c3e8a687dea490e3d8
mh78169898-netizen <mh78169898@gmail.com>
2025-11-17 15:23:42 +0900

    Update 문자열 작성.cpp

--- C++/Chap01/문자열 작성.cpp
+++ C++/Chap01/문자열 작성.cpp
@@ -10,4 +10,21 @@ int main()
     //인삿말 작성
     std::cout << "Hello, " << name << "!" << std::endl;
 }
+------------------------최신 문법 ver--------------------------
+#include <iostream>
+#include <string>
+
+
+
+int main()
+{
+    using namespace std;
+
+    cout << "당신의 첫번째 이름을 입력하세요 : ";
+    string name;//string 변수 선언
+    cin >> name;
+
+    //인삿말 작성
+    cout << "Hello, " << name << "!" << endl;
+}
 
