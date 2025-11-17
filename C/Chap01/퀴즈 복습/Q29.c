c9516370764a7c2880b5fc2133da8575b3ee7786
mh78169898-netizen <mh78169898@gmail.com>
2025-11-17 10:58:33 +0900

    Create Q29.c

---
+++ C/Chap01/퀴즈 복습/Q29.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *ifp, *ofp;
+    char name[20];
+    int kor, eng, math;
+    int total;
+    double avg;
+    int res;
+
+    ifp = fopen("a2.txt", "r");
+    if(ifp == NULL){
+        printf("입력 파일을 열지 못했습니다.\n");
+        return 1;
+    }
+
+     ofp = fopen("b2.txt", "w");
+    if(ofp == NULL){
+        printf("입력 파일을 열지 못했습니다.\n");
+        return 1;
+    }
+
+    while(1){
+         res = fscanf(ifp, "%s%d%d%d", name, &kor, &eng, &math);
+        if(res == EOF){ 
+            break;
+        }
+        total = kor + eng + math;
+        avg = total / 3.0;
+        fprintf(ofp, "%s%5d%7.1lf\n", name, total, avg);
+    }
+
+    fclose(ifp);
+    fclose(ofp);
+
+    return 0;
+}
