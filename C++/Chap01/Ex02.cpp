a1967af2525bc20b61c66c8b9582a5228b54eda0
mh78169898-netizen <mh78169898@gmail.com>
2025-11-18 11:39:42 +0900

    Create Ex02.cpp

---
+++ C++/Chap01/Ex02.cpp
@@ -0,0 +1,11 @@
+//bool 타입 사용
+#include <iostream>
+
+int main()
+{
+    bool b1 = true;
+    bool b2 = false;
+
+    std::cout << "b1 = " << b1 << "\n" << std::endl;
+    std::cout << "b2 = " << b2 << "\n" << std::endl;
+}
