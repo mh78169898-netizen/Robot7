bfd1e81d8db1a80ef1090709feba19c13827bb4b
mh78169898-netizen <mh78169898@gmail.com>
2025-11-18 11:43:12 +0900

    Update Ex02.cpp

--- C++/Chap01/Ex02.cpp
+++ C++/Chap01/Ex02.cpp
@@ -9,3 +9,11 @@ int main()
     std::cout << "b1 = " << b1 << "\n" << std::endl;
     std::cout << "b2 = " << b2 << "\n" << std::endl;
 }
+
+//묵시적,명시적인 형변환
+
+    int a = 100;
+    double b = 3.14;
+
+    b = a;// 묵시적인 형변환
+    a = (int)b;//명시적인 형변환
