6e0b8ef217de9d06481763bd022cf65657fba9f2
mh78169898-netizen <mh78169898@gmail.com>
2025-11-19 15:14:54 +0900

    Create Ex03_04.cpp

---
+++ C++/Chap01/3장/Ex03_04.cpp
@@ -0,0 +1,27 @@
+//위임 생성자 호출
+#include <iostream>
+using namespace std;
+
+class Circle{
+public:
+    //1.멤버 변수
+     int rid;
+    //2.생성자
+    Circle():rid(1)//초기화 변수
+    { 
+        cout << "circle의 반지름 : " << rid << endl;
+    }
+    Circle(int r):rid(r)
+    {
+        cout << "circle의 반지름 : " << rid << endl;
+    }
+    //3.멤버 메소드
+};
+
+int main()
+{    
+    Circle donut;
+    Circle pizza(30);
+
+    return 0;
+}
