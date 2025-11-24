3ed558dd7c996af99de5f016b0ab54af790aed0a
mh78169898-netizen <mh78169898@gmail.com>
2025-11-24 16:38:33 +0900

    Update Q36.cpp

--- C++/Chap01/퀴즈 복습/Q36.cpp
+++ C++/Chap01/퀴즈 복습/Q36.cpp
@@ -13,15 +13,18 @@ int main()
     for(int i = 0; i < 5; i++){
         cout << i + 1 << ">>";
         getline(cin, name);
-        sv.push_back(name);
+        sv.push_back(name); //벡터 메모리(요소) 추가
     }
-    sort(sv.begin(),sv.end());
+    sort(sv.begin(),sv.end()); //
 
     vector<string>::iterator it;
    
-    for(it = sv.begin(); it != sv.end(); it++){
-        cout << *it << ' ';
-    }
+    // for(it = sv.begin(); it != sv.end(); it++){
+    //     cout << *it << ' ';
+    // }
+    for(string s : sv){
+        cout << s << "   ";
+    } // for each 구문
     cout << endl;
     return 0;
 }
