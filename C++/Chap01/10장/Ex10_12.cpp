2edea617602dfa8c192736536af24b3add915f4b
mh78169898-netizen <mh78169898@gmail.com>
2025-11-25 11:55:12 +0900

    Create Ex10_12.cpp

---
+++ C++/Chap01/10장/Ex10_12.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main()
+{
+    map<string, string> dic;
+
+    dic.insert(make_pair("love", "사랑"));//함수로 삽입
+    //dic["love"] = "사랑"; //key에 값을 직접 삽입
+
+    string kor = dic["love"]; //key를 준다
+    string kor2 = dic.at("love"); //key를 주면 값을 kor2로 보낸다
+
+    cout << kor << endl; //value가 출력됨
+    cout << kor2 << endl; //value가 출력됨
+
+    
+    return 0;
+}
