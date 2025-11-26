7388af4e2e826a9c5be0cc20279c6ef3e7b4be1f
mh78169898-netizen <mh78169898@gmail.com>
2025-11-26 11:26:12 +0900

    Create Q44.cpp

---
+++ C++/Chap01/퀴즈 복습/Q44.cpp
@@ -0,0 +1,26 @@
+//부분은 list를 사용할때 적용시키는 코드
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+//#include <list>
+using namespace std;
+
+int main() {
+    vector<string> sv;
+    //list<string> list;
+	string names;
+	for(int i=0; i<5; i++) {
+        cout << "이름 >> " << endl;
+		getline(cin, names);
+        sv.push_back(names);
+        //list.push_back(names);
+	}
+    sort(sv.begin(), sv.end());
+    //list.sort();//오름차순,  list.sort(geater<string>())// 내림차순
+    string latter = sv.back();
+
+    cout << "사전에서 가장 뒤에 나오는 문자열은 " << latter << endl;
+    //cout << list.back() << endl;
+    return 0;
+}
