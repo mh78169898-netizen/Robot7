fbec291f5725166913e7953081a27a43795000a9
mh78169898-netizen <mh78169898@gmail.com>
2025-12-01 14:54:03 +0900

    디폴트 매개 변수로 인한 함수 중복의 모호성

---
+++ C++/Chap01/6장/Ex06_09.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void msg(int od)(
+    cout << id << endl;
+)
+
+void msg(int id, string s = ""){
+    cout << id << ":" << s << endl;
+}
+
+int main()
+{
+    msg(5, "Good Moning");
+    msg(6);
+    return 0;
+}
