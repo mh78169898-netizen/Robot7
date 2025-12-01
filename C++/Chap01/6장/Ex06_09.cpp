29a08b5a4095677b3cd58e60173e5222bb7055c5
mh78169898-netizen <mh78169898@gmail.com>
2025-12-01 15:01:17 +0900

    디폴트 매개 변수로 인한 함수 중복의 모호성

--- C++/Chap01/6장/Ex06_09.cpp
+++ C++/Chap01/6장/Ex06_09.cpp
@@ -2,17 +2,18 @@
 #include <string>
 using namespace std;
 
-void msg(int od)(
+void msg(int id){
     cout << id << endl;
-)
+}
 
 void msg(int id, string s = ""){
     cout << id << ":" << s << endl;
 }
 
-int main() 
+int main()
 {
     msg(5, "Good Moning");
     msg(6); //디폴트 매개변수 --> 매개 변수를 2개 사용한 것!!!
     return 0;
 }
+
