1232af364823020b2bf7a97bfae7006be82e8978
mh78169898-netizen <mh78169898@gmail.com>
2025-12-03 14:36:44 +0900

    Update Q50.cpp

--- C++/Chap01/퀴즈 복습/Q50.cpp
+++ C++/Chap01/퀴즈 복습/Q50.cpp
@@ -43,3 +43,49 @@ int main()
 
     return 0;
 }
+
+------코드 약간 수정한 버전------
+#include <iostream>
+#include <memory>
+using namespace std;
+
+class Calculator
+{
+public:
+    //순수 가상함수 3개를 만드시오
+    virtual int add(int a, int b) = 0;
+    virtual int subtract(int a, int b) = 0;
+    virtual double average(int a[], int size) = 0;
+};
+
+class GoodCalc : public Calculator
+{
+public:
+    int add(int a, int b) override{
+        return a + b;
+    }
+    int subtract(int a, int b) override{
+        return a - b;
+    }
+    double average(int a[], int size) override{
+        double sum = 0;
+        for(int i = 0; i < size; i++){
+            sum += a[i];
+            
+        }
+        return sum / size;
+    }
+
+};
+
+
+int main()
+{
+    int a[] = {1,2,3,4,5};
+    auto p = make_unique<GoodCalc>();
+    cout << p->add(2, 3) << endl;
+    cout << p->subtract(2, 3) << endl;
+    cout << p->average(a, 5) << endl;
+
+    return 0;
+}
