385294d1970b59aa1616287fa31112b4e926cd00
mh78169898-netizen <mh78169898@gmail.com>
2025-12-10 12:39:17 +0900

    숫

--- Python/ch01/06-1/에외처리2.py
+++ Python/ch01/06-1/에외처리2.py
@@ -6,7 +6,7 @@ for item in list_input_a: #숫자로 변환 후 리스트에 추가
         float(item)
         list_number.append(item) #예외 없이 통과하면 다음으로 진행
     except:
-        pass  #예외가 발생하면 자동으로 종료
+        pass  #예외가 발생하면 아무것도 안핟고 통과
 
 print("{} 내부에 있는 숫자는".format(list_input_a))
 print("{} 입니다.".format(list_number))
